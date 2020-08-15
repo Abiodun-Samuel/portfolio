b95bb81033d02101779afb9f49f770eb6fba1b2e
Abiodun-Samuel <69618294+Abiodun-Samuel@users.noreply.github.com>
2020-08-15 06:31:41 +0100

    Update script.js
    updated clean version

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// ========== scroll effects ==========
+// ========== Navbar scroll effects ==========
 
 window.onscroll = function () {
     myFunction();
@@ -12,43 +12,25 @@ function myFunction() {
     } else {
         nav.classList.remove("sticky");
     }
-}
+};
 
-$(document).ready(function () {
-    $(".navbar-toggle").on("click", function () {
-        $(this).toggleClass("active");
-    });
-});
+// $(document).ready(function () {
+//     $(".navbar-toggle").on("click", function () {
+//         $(this).toggleClass("active");
+//     });
+// });
 
-// ========== scroll effects ==========
 
-// ==========typing effect========== 
+
+
+// ==========type js typing effect========== 
 
 var typed = new Typed('.type', {
-    strings: ['Full Stack Web Developer', 'UI-UX Designer', 'Digital Marketer'],
+    strings: ['Web Developer', 'UI-UX Designer', 'Digital Marketer'],
     typeSpeed: 60,
     backSpeed: 60,
     loop: true,
 });
 
 // ==========typing effect ends here==========
-// function OnImageLoaded(img) {
-//     alert("this image has been loaded:" + img.src);
-// }
-// function PreLoadImage(src) {
-//     var img = new Image();
-//     img.onload = function () {
-//         OnImageLoaded(this)
-//     };
-//     img.src = src;
-// }
-// PreLoadImage("contact-background.jpg");
-
-
-
-// ========== scroll effects ==========
-// window.addEventListener("scroll", function () {
-//     var header = document.getElementById("header");
-//     header.classList.toggle("sticky", window.scrollY > 0);
-// })
 
