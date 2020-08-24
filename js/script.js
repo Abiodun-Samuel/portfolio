7421b2bb799ab720a883ddc78c5d07fef7bae01d
Abiodun-Samuel <69618294+Abiodun-Samuel@users.noreply.github.com>
2020-08-24 07:17:34 +0100

    Add files via upload
    scroll bar progress added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// scroll progress bar
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: { scrub: 0.3 }
+});
+
+// scroll progress bar 
+
 // ========== Nav Toggle effects ==========
 
 const navSlide = () => {
